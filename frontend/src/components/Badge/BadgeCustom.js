0a331ee6d9e9b3be69160f200e219ea87a1c879a
nhiepchau <nhiepbaochau@gmail.com>
2023-10-11 01:38:45 +0700

    Minors

--- frontend/src/components/Badge/BadgeCustom.js
+++ frontend/src/components/Badge/BadgeCustom.js
@@ -5,9 +5,9 @@ const BadgeCustom = ({ value, onPress, style, styleText }) => {
 	return (
 		<TouchableOpacity
             onPress= { onPress }
-            className={"bg-primary-lighter mr-2 rounded-normal px-2 ' + style}
+            className={"bg-primary-lighter mr-2 rounded-normal px-2" + style}
         >
-            <Text className='text-center text-primary-darker" style={ styleText }>
+            <Text className='text-center text-primary-darker' style={ styleText }>
 			    {value}
 		    </Text>
         </TouchableOpacity>
