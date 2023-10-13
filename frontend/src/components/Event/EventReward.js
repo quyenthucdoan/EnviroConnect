c3b2609c716c473240cda7cec240ae783d0980fd
nhiepchau <nhiepbaochau@gmail.com>
2023-10-14 02:59:55 +0700

    add activityStore + improve image activity

--- frontend/src/components/Event/EventReward.js
+++ frontend/src/components/Event/EventReward.js
@@ -3,7 +3,14 @@ import { View, Text } from "react-native"
 const EventReward = () => {
 	return (
 		<View className="h-full bg-white px-2 pt-5">
-			<Text>REWARD TAB</Text>
+			<Text className="font-semibold text-normal">Benefits</Text>
+			<View className="mt-3 bg-primary-lighter rounded border-primary-light border-primary py-2 px-3">
+				<Text className="font-semibold text-small">Point</Text>
+				<View className="flex flex-row gap-x-5 mt-3">
+					<Text className="text-small text-primary-normal">2 social days</Text>
+					<Text className="text-small text-primary-normal">100 points</Text>
+				</View>
+			</View>
 		</View>
 	)
 }
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import locationReducer from "./reducers/locationSlice"
+import activityListReducer from "./reducers/activitySlice"
 
 export default store = configureStore({
 	reducer: {
 		location: locationReducer.reducer,
+		activityList: activityListReducer.reducer
 	},
 })
